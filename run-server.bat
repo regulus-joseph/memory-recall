50678786a4abda66d7a46fc943c9bafbfc80f8ca
regulus-joseph <zixuan_wei@hotmail.com>
2026-04-24 16:21:28 +0800

    chore: remove temp patch files

--- run-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-title Memory Recall Server
-echo Starting Memory Recall Server...
-echo Data dir: %USERPROFILE%\.memory-recall\data
-echo Qdrant: localhost:6333
-echo Ollama: localhost:11434
-echo.
-python -m src.server
